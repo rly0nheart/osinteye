132dd5af256cdcc9ce486e52155ad174a7b674f3
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-02-22 01:40:09 +0200

    added pypi.py for v1.1.0

---
+++ plugins/pypi.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+from lib.colors import red,white,reset
+
+def pypi(self,args):
+	response = requests.get(self.url)
+	if response.status_code != 200:
+		exit(f"{white}[{red}-{white}] github: user not found{reset} ")
+	soup = BeautifulSoup(response.text, 'html.parser')
+	print('\n\t'+soup.title.string)
+	print(soup.main.get_text())
