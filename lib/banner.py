1cf4a2cac63eb89f7d3fa6fdfe4cb3fed331bd21
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-02-22 02:47:32 +0200

    Update banner.py

--- lib/banner.py
+++ lib/banner.py
@@ -1,8 +1,8 @@
 from lib.colors import red,white,reset
 
 banner = f''''
-{white}┏━━━┳━┳┓╋╋┏┓  {red}┏━┓              
-{white}┃┏━┓┃━╋╋━┳┫┗  {red}┫┳╋┳┳━┓   
-{white}┃┃┃┃┣━┃┃┃┃┃┏  {red}┫┻┫┃┃┻┫   
-{white}┃┃┃┃┣━┻┻┻━┻━  {red}┻━╋┓┣━┛   
-{white}┃┗━┛┃╋╋╋╋╋╋╋  {red}╋╋┗━┛v1.1.0  {reset}'''
+{white}┏━━━┳━┳┓╋╋┏┓{red}┏━┓              
+{white}┃┏━┓┃━╋╋━┳┫┗{red}┫┳╋┳┳━┓   
+{white}┃┃┃┃┣━┃┃┃┃┃┏{red}┫┻┫┃┃┻┫   
+{white}┃┃┃┃┣━┻┻┻━┻━{red}┻━╋┓┣━┛   
+{white}┃┗━┛┃╋╋╋╋╋╋╋{red}╋╋┗━┛v1.1.0  {reset}'''
