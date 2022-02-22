dd19faa9d7867fa6da33fd49d1d35defc9855028
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-02-22 15:08:37 +0200

    Update pypi.py

--- plugins/pypi.py
+++ plugins/pypi.py
@@ -5,7 +5,7 @@ from lib.colors import red,white,reset
 def pypi(self,args):
 	response = requests.get(self.url)
 	if response.status_code != 200:
-		exit(f"{white}[{red}-{white}] github: user not found{reset} ")
+		exit(f"{white}[{red}-{white}] pypi: user not found{reset} ")
 	soup = BeautifulSoup(response.text, 'html.parser')
 	print('\n\t'+soup.title.string)
 	print(soup.main.get_text())
